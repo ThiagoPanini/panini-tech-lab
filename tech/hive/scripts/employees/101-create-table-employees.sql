fd857574d6414867852581f0972f13e8f6de7d4f
Thiago Panini <panini.development@gmail.com>
2022-05-09 22:32:50 -0300

    Iniciando a construção de scripts HiveQL

---
+++ tech/hive/scripts/employees/101-create-table-employees.sql
@@ -0,0 +1,9 @@
+# Definindo variáveis
+SET hivevar:database='paninitechlab';
+SET hivevar:table='employees';
+
+# Criando banco de dados
+CREATE DATABASE IF NOT EXISTS 
+
+# Criando tabela
+CREATE TABLE IF NOT EXISTS ${database}.${table}
