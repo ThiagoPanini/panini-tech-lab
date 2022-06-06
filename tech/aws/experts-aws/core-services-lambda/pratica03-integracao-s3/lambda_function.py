3789168a26fd56286ed2b702b9a723cd14fd2361
Thiago Panini <panini.development@gmail.com>
2022-06-05 23:21:50 -0300

    Consolidando recursos da terceira prática sobre Lambda

--- tech/aws/experts-aws/core-services-lambda/pratica03-integracao-s3/lambda_function.py
+++ tech/aws/experts-aws/core-services-lambda/pratica03-integracao-s3/lambda_function.py
@@ -1,9 +1,11 @@
 # Importando bibliotecas
 import boto3
+import json
+import os
 
 # Definindo variáveis do projeto
 BUCKET = 'aws-experts-dx6sdjz2j7ro-sa-east-1'
-KEY = 'lambda/tests/rock_albuns.csv'
+KEY = 'lambda/input/rock_albuns.csv'
 
 def lambda_handler(event, context):
     
@@ -17,3 +19,23 @@ def lambda_handler(event, context):
     print(data)
     
     return None
+
+
+"""
+---------------------------------------------------
+------------ BLOCO DE TESTES DA FUNÇÃO ------------
+     Configurando eventos e testando execução
+---------------------------------------------------
+"""
+
+# Definindo variáveis para leitura de evento de teste
+LAMBDA_PATH = os.path.join(os.getcwd(), 'tech/aws/experts-aws/core-services-lambda')
+FUNCTION_REF = 'pratica03-integracao-s3'
+EVENT_PATH = 'resources/tests/test-event.json'
+
+# Lendo evento de teste
+with open(os.path.join(LAMBDA_PATH, FUNCTION_REF, EVENT_PATH)) as f:
+    event = json.load(f)
+
+if __name__ == '__main__':
+    lambda_handler(event=event, context=None)
