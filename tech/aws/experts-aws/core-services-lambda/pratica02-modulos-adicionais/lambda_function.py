b63fd61d32f9733e97cf16a681c64a29d89e3349
Thiago Panini <panini.development@gmail.com>
2022-06-09 23:12:11 -0300

    Adicionando policy IAM e evento de testes no diretório de recursos da função

--- tech/aws/experts-aws/core-services-lambda/pratica02-modulos-adicionais/lambda_function.py
+++ tech/aws/experts-aws/core-services-lambda/pratica02-modulos-adicionais/lambda_function.py
@@ -2,6 +2,7 @@
 import json
 from utils.log import log_config
 import logging
+import os
 
 # Função para retornar os campos do schema
 def get_schema(schema, key='fields'):
@@ -36,3 +37,23 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(','.join(fields))
     }
+
+
+"""
+---------------------------------------------------
+------------ BLOCO DE TESTES DA FUNÇÃO ------------
+     Configurando eventos e testando execução
+---------------------------------------------------
+"""
+
+# Definindo variáveis para leitura de evento de teste
+LAMBDA_PATH = os.path.join(os.getcwd(), 'tech/aws/experts-aws/core-services-lambda')
+FUNCTION_REF = 'pratica02-modulos-adicionais'
+EVENT_PATH = 'resources/tests/test-event.json'
+
+# Lendo evento de teste
+with open(os.path.join(LAMBDA_PATH, FUNCTION_REF, EVENT_PATH)) as f:
+    event = json.load(f)
+
+if __name__ == '__main__':
+    lambda_handler(event=event, context=None)
