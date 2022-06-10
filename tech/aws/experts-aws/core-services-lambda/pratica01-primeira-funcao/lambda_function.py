bf7903b69ca5fe74eae237191e7bc0b13578f4e4
Thiago Panini <panini.development@gmail.com>
2022-06-09 23:06:35 -0300

    Adicionando policy do IAM e evento de testes da função

--- tech/aws/experts-aws/core-services-lambda/pratica01-primeira-funcao/lambda_function.py
+++ tech/aws/experts-aws/core-services-lambda/pratica01-primeira-funcao/lambda_function.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def lambda_handler(event, context):
     
@@ -11,3 +12,24 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Voce esta ouvindo sua musica preferida!')
     }
+
+"""
+---------------------------------------------------
+------------ BLOCO DE TESTES DA FUNÇÃO ------------
+     Configurando eventos e testando execução
+---------------------------------------------------
+"""
+
+# Definindo variáveis para leitura de evento de teste
+LAMBDA_PATH = os.path.join(os.getcwd(), 'tech/aws/experts-aws/core-services-lambda')
+FUNCTION_REF = 'pratica01-primeira-funcao'
+EVENT_PATH = 'resources/tests/test-event.json'
+
+# Lendo evento de teste
+with open(os.path.join(LAMBDA_PATH, FUNCTION_REF, EVENT_PATH)) as f:
+    event = json.load(f)
+
+if __name__ == '__main__':
+    print(os.getcwd())
+    print(os.listdir())
+    lambda_handler(event=event, context=None)
