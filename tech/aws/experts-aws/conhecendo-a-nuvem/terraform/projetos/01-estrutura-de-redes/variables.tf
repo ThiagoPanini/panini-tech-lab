8f0eb0dd95415dc26a75b275eb2f2e42978b1f2b
Thiago Panini <panini.development@gmail.com>
2022-06-27 11:52:42 -0300

    Finalizando script de variáveis do módulo root

--- tech/aws/experts-aws/conhecendo-a-nuvem/terraform/projetos/01-estrutura-de-redes/variables.tf
+++ tech/aws/experts-aws/conhecendo-a-nuvem/terraform/projetos/01-estrutura-de-redes/variables.tf
@@ -21,3 +21,31 @@ variable "aws_provider_config" {
     "credentials" = ["~/.aws/credentials"]
   }
 }
+
+# Definição de bloco CIDR a ser utilizado na criação da VPC
+variable "vpc_cidr_block" {
+  description = "Bloco cidr que define o intervalo de endereços de ip da vpc"
+  type        = string
+  default     = "172.11.0.0/16"
+}
+
+# ARN da managed policy da AWS responsável por tais permissões
+variable "ssm_policy_arn" {
+  description = "ARN da política gerenciada pela AWS que permite conexões de uma EC2 via SSM"
+  type        = string
+  default     = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Definindo ami da instância
+variable "instance_ami" {
+  description = "Identificação da AMI utilizada para a instância EC2"
+  type        = string
+  default     = "ami-0cff7528ff583bf9a"
+}
+
+# Tipo da instância
+variable "instance_type" {
+  description = "Tipo da instância a ser construída e implantada"
+  type        = string
+  default     = "t2.micro"
+}
