d861c0c71b878d046221251ac57059f1b0e7223a
Thiago Panini <panini.development@gmail.com>
2022-06-26 22:37:03 -0300

    Declarando variáveis para o módulo root e adicionando documentação no arquivo

--- tech/aws/experts-aws/conhecendo-a-nuvem/terraform/projetos/01-estrutura-de-redes/variables.tf
+++ tech/aws/experts-aws/conhecendo-a-nuvem/terraform/projetos/01-estrutura-de-redes/variables.tf
@@ -1,9 +1,23 @@
-# Variável com parâmetros de configuração
+/* --------------------------------------------------
+FILE: variables.tf @ root module
+
+CONTEXT: Arquivo de especificação de variáveis a serem
+utilizadas no módulo root desta especificação de infra
+
+GOAL: O objetivo deste arquivo é centralizar a declaração
+de variáveis importantes para o projeto, se tornando 
+então uma foram de agilizar o desenvolvimento do código
+através de um local organizado para uso das variáveis.
+As variáveis alocadas neste arquivo são de uso 
+exclusivo do arquivo main.tf no módulo root.
+-------------------------------------------------- */
+
+# Variável utilizada para autenticação e configuração do provedor AWS
 variable "aws_provider_config" {
-    description = "Caminhos de configuração e credenciais do provedor AWS"
-    type = map
-    default = {
-        "config" = ["~/.aws/config"]
-        "credentials" = ["~/.aws/credentials"]
-    }
+  description = "Caminhos de configuração e credenciais do provedor AWS"
+  type        = map(any)
+  default = {
+    "config"      = ["~/.aws/config"]
+    "credentials" = ["~/.aws/credentials"]
+  }
 }
