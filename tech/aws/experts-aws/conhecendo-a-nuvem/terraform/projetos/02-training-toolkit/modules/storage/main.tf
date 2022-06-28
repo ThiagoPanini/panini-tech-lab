5fbb4607cb65fac10bfc377149d42cfa54ba1c11
Thiago Panini <panini.development@gmail.com>
2022-06-27 23:44:51 -0300

    Construindo lógica para ingestão local de dados no s3

---
+++ tech/aws/experts-aws/conhecendo-a-nuvem/terraform/projetos/02-training-toolkit/modules/storage/main.tf
@@ -0,0 +1,66 @@
+/* --------------------------------------------------
+FILE: main.tf @ storage module
+
+CONTEXT: Arquivo principal de construção de parte
+específica da infraestrutura cabível ao contexto do
+módulo em questão.
+
+GOAL: O objetivo deste arquivo é consolidar a criação
+de um bucket s3 alvo de todo o armazemaneto dos
+objetos a serem utilizados no toolkit de capacitação
+aqui proposto.
+
+RESOURCES: Os recursos aqui implantados serão:
+  - Bucket S3
+-------------------------------------------------- */
+
+# Definindo bucket s3
+resource "aws_s3_bucket" "private_training_bucket" {
+  bucket        = var.bucket_name
+  force_destroy = var.enable_force_destroy
+}
+
+# Definindo bloqueio de acesso público ao bucket
+resource "aws_s3_bucket_public_access_block" "all_private" {
+  bucket = aws_s3_bucket.private_training_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Criptografando bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.private_training_bucket.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
+# Criando diretórios para o bucket
+resource "aws_s3_object" "bucket_folders" {
+  for_each = toset(var.bucket_folders)
+  bucket   = aws_s3_bucket.private_training_bucket.bucket
+  key      = each.key
+}
+
+# Realizando o upload de bases de dados
+resource "aws_s3_object" "bucket_data_sources" {
+  for_each = fileset(var.local_upload_data_path, "**")
+  bucket   = aws_s3_bucket.private_training_bucket.bucket
+  key      = "data/${each.value}"
+  source   = "${var.local_upload_data_path}${each.value}"
+  etag     = filemd5("${var.local_upload_data_path}${each.value}")
+}
+
+# Realizando o upload de funções lambda
+resource "aws_s3_object" "bucket_lambda_functions" {
+  for_each = fileset(var.local_upload_lambda_path, "**")
+  bucket   = aws_s3_bucket.private_training_bucket.bucket
+  key      = "resources/lambda/${each.value}"
+  source   = "${var.local_upload_lambda_path}${each.value}"
+  etag     = filemd5("${var.local_upload_lambda_path}${each.value}")
+}
---
+++ tech/aws/experts-aws/conhecendo-a-nuvem/terraform/projetos/02-training-toolkit/modules/storage/variables.tf
@@ -0,0 +1,36 @@
+/* --------------------------------------------------
+FILE: variables.tf @ storage module
+
+CONTEXT: Arquivo de declaração de variáveis a ser 
+utilizado nos recursos criados especificamente neste
+módulo.
+
+GOAL: O objetivo deste arquivo é concentrar a declaração
+de variáveis cabíveis ao contexto da instância EC2
+a ser construída em subnet privada e conexão via SSM
+-------------------------------------------------- */
+
+variable "bucket_name" {
+  description = "Nome do bucket s3 a ser criado para consolidação dos objetos do tookit de capacitação"
+  type        = string
+}
+
+variable "enable_force_destroy" {
+  description = "Habilita eliminação de objetos do bucket na aplicação do terraform destroy"
+  type        = bool
+}
+
+variable "bucket_folders" {
+  description = "Lista de folders a serem criados no bucket alvo para posterior recepção dos objetos"
+  type        = list(string)
+}
+
+variable "local_upload_data_path" {
+  description = "Caminho de armazenamento dos dados locais que serão inseridos no s3"
+  type        = string
+}
+
+variable "local_upload_lambda_path" {
+  description = "Caminho local de armazenamento das funções lambda que serão inseridas no s3"
+  type        = string
+}
