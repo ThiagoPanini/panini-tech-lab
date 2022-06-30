a5dac44f6d740420ae3177ea9d51ab74524059ff
Thiago Panini <panini.development@gmail.com>
2022-06-29 22:18:24 -0300

    Construindo vpc, subnet, rt e security group

---
+++ tech/aws/experts-aws/conhecendo-a-nuvem/terraform/projetos/02-training-toolkit/modules/network/outputs.tf
@@ -0,0 +1,32 @@
+/* --------------------------------------------------
+FILE: outputs.tf @ network module
+
+CONTEXT: Arquivo de definição de outputs a serem usados
+em arquivos externos à este módulo.
+
+GOAL: O objetivo deste arquivo é expor algumas variáveis,
+atributos ou elementos criados neste módulo de redes
+para serem utilizados em outros módulos ou mesmo na 
+especificação da infraestrutura no arquivo de
+configuração root. De maneira prática, o ID da VPC ou
+bloco cidr podem ser informações úteis a serem utilizados
+em uma série de outros recursos declarados no projeto.
+-------------------------------------------------- */
+
+# Exportando id da vpc como saída
+output "vpc_id" {
+  value       = aws_vpc.project.id
+  description = "Id da vpc criada no módulo network"
+}
+
+# Exportando cidr block da vpc
+output "vpc_cidr_block" {
+  value       = aws_vpc.project.cidr_block
+  description = "Id da vpc criada no módulo network"
+}
+
+# Exportando informações das subnets
+output "subnet_id" {
+  description = "Id de subnet privada para definição de regras em outros módulos"
+  value       = aws_subnet.private.id
+}
---
+++ tech/aws/experts-aws/conhecendo-a-nuvem/terraform/projetos/02-training-toolkit/modules/network/variables.tf
@@ -0,0 +1,19 @@
+/* --------------------------------------------------
+FILE: variables.tf @ network module
+
+CONTEXT: Arquivo de declaração de variáveis a ser 
+utilizado nos recursos criados especificamente neste
+módulo.
+
+GOAL: O objetivo deste arquivo é concentrar a declaração
+de variáveis cabíveis ao contexto de redes para os
+recursos implantados no módulo main.tf. Entre as
+variáveis consideradas, será possível encontrar
+blocos CIDR para facilitar a definição do intervalo
+de endereços de uma VPC e de suas respectivas subnets.
+-------------------------------------------------- */
+
+variable "vpc_cidr_block" {
+  description = "Bloco cidr que define o intervalo de endereços de ip da vpc"
+  type        = string
+}
