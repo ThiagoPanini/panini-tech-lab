532302889468c033c4934e112995c779dd9d83e6
Thiago Panini <panini.development@gmail.com>
2022-06-29 21:28:44 -0300

    Criando arquivo de outputs do módulo iam para associação de roles às funções lambda

--- tech/aws/experts-aws/conhecendo-a-nuvem/terraform/projetos/02-training-toolkit/modules/iam/outputs.tf
+++ tech/aws/experts-aws/conhecendo-a-nuvem/terraform/projetos/02-training-toolkit/modules/iam/outputs.tf
@@ -9,7 +9,37 @@ recursos definidos neste projeto possam utilizar
 policies e roles IAM aqui descritas.
 -------------------------------------------------- */
 
-output "role_lambda_101" {
-  description = "Role iam para função experts-aws-lambda-role-101"
-  value       = aws_iam_role.lambda-101.arn
+output "lambda-basic-role" {
+  description = "Role iam com permissões básicas de log para funções lambda"
+  value       = aws_iam_role.lambda-basic-role.arn
+}
+
+output "lambda-103" {
+  description = "Role iam com permissões de coleta de arquivo csv de bucket s3"
+  value       = aws_iam_role.lambda-103.arn
+}
+
+output "lambda-104" {
+  description = "Role iam com permissões de coleta de arquivo csv de bucket s3"
+  value       = aws_iam_role.lambda-104.arn
+}
+
+output "lambda-105" {
+  description = "Role iam com permissões para ligar e desligar instancias ec2"
+  value       = aws_iam_role.lambda-105.arn
+}
+
+output "lambda-106" {
+  description = "Role iam com permissões de deletar volumes ebs em uma conta aws"
+  value       = aws_iam_role.lambda-106.arn
+}
+
+output "lambda-107" {
+  description = "Role iam com permissões de coleta de arquivo csv de bucket s3"
+  value       = aws_iam_role.lambda-103.arn
+}
+
+output "lambda-108" {
+  description = "Role iam com permissões de coleta de arquivo csv de bucket s3"
+  value       = aws_iam_role.lambda-103.arn
 }
